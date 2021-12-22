ce29af5083f02d10e39fb00224259799e94fb696
@nigjar <94608936+Nigjar@users.noreply.github.com>
2021-12-22 11:01:27 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Skeleton",
+
+    'summary': """
+        This module is a skeleton for our module development""",
+
+    'description': """
+        TODO: Long description of module's purpose
+    """,
+
+    'author': "42 N.E.R.D.S.",
+    'website': "https://www.42nerds.com",
+    'category': 'Extra Tools',
+    'version': '0.0.13',
+    # 'depends': [
+    # ],
+    # 'data': [
+    # ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
